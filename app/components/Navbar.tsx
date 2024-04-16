cba11ad8d80780e42930d91f0140151fdab9576b
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-04-17 00:51:33 +0530

    Navbar and auth completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,31 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
 import reddit from '../../public/reddit.png'
-import redddittext from '../../public/redddittext.png'
+import redtext from '../../public/redtext.png'
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import UserDropdown from "./UserDropdown";
 
-export  function Navbar() {
+
+export  async function Navbar() {
+
+  const {getUser}=getKindeServerSession()
+  const user=await getUser();
+    // console.log("userDetails",user);
+    
   return (
-    <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between">
-    <Link href='/' className="flex items-center gap-x-0">
+    <nav className="h-[10vh] w-full flex items-center border px-5 lg:px-14 justify-between">
+    <Link href='/' className="flex items-center gap-x-3">
       <Image src={reddit}
       alt="reddit-logo"
       className="h-10 w-fit"
       />
 
-      <Image src={redddittext}
+      <Image src={redtext}
       alt="redditText"
-      className="h-[70px] w-fit hidden lg:block "
+      className="h-7  w-fit hidden lg:block "
       />
     
-    
-    
-    
-    
-    </Link>
+     </Link>
+
+    <div className="flex items-center gap-x-4">
+    <ThemeToggle/>
+
+    {user ? (
+
+    <UserDropdown userImage={user.picture}/>
 
 
+    ):(
+      <div className="flex items-center gap-x-4">
 
+      <Button variant="secondary" asChild><RegisterLink>Sign up</RegisterLink></Button>
+      <Button asChild><LoginLink>Login in</LoginLink></Button>
 
+      </div>
+    )}
+
+    </div>
 
 
     </nav>
---
+++ app/components/UserDropdown.tsx
@@ -0,0 +1,50 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+import {LogoutLink} from '@kinde-oss/kinde-auth-nextjs/components'
+
+interface isprop {
+    userImage: string | null
+}
+55
+  
+export default function UserDropdown({userImage}: isprop) {
+  return (
+    <DropdownMenu>
+   <DropdownMenuTrigger>
+    <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+     <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5"/>
+     <img src= { userImage ??"https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" }
+     alt="user avatar"
+     className="rounded-full h-8 w-8 hidden lg:block"
+     />
+    </div>
+
+   </DropdownMenuTrigger>
+
+
+   <DropdownMenuContent align="end" className="w-[200px]">
+    <DropdownMenuItem>
+      <Link href='/r/create' className="w-full">
+       Create Community
+      </Link>
+
+    </DropdownMenuItem>
+    <DropdownMenuItem>
+    <Link href='/create' className="w-full">
+       Create Post
+      </Link>
+    </DropdownMenuItem>
+    <DropdownMenuItem>
+    <Link href='/settings' className="w-full">
+       Settings
+      </Link>
+    </DropdownMenuItem>
+    <DropdownMenuSeparator/>
+    <DropdownMenuItem>
+       <LogoutLink>Logout</LogoutLink>
+    </DropdownMenuItem>
+   </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
