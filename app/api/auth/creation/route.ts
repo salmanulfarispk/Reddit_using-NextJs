71de96f2f67ab00aa8084c3863107565da1d0d98
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-04-17 15:15:14 +0530

    auth with kind and storing datas are completed

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,4 +1,7 @@
+import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+import { generateUsername } from "unique-username-generator";
 
 
 
@@ -10,4 +13,29 @@ export async function GET(){
     if(!user || user === null || !user.id)
      throw new Error("something went wrong ,please try again")
     
+    let dbuser=await prisma.user.findUnique({
+        where:{
+            id:user.id,
+
+        },
+    });
+     
+    if(!dbuser){
+      const dbuser=await prisma.user.create({
+        data: {
+            id:user.id,
+            email:user.email ?? "",
+            firstName:user.given_name  ?? "",
+            lastName :user.family_name ?? "",
+            imageUrl: user.picture,
+            userName : generateUsername("-", 3, 15)  //- as a seperator and 3 as three random digits and 15 as total lenght
+
+        }
+      });
+
+    }
+
+    return NextResponse.redirect('http://localhost:3000/')
+
+
 }
